11a0808e83587f657f2a5ecbe5b58c8356c39c9c
CANCELLARO GIANMICHELE <gnmcl01@outlook.it>
2026-04-14 00:33:55 +0200

    feat: reorganize imports in SecurityConfig and add HealthController for health check endpoint

--- src/main/java/com/prenota24/backend/config/SecurityConfig.java
+++ src/main/java/com/prenota24/backend/config/SecurityConfig.java
@@ -1,8 +1,7 @@
 package com.prenota24.backend.config;
 
-import com.prenota24.backend.auth.JwtAuthenticationEntryPoint;
-import com.prenota24.backend.auth.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,7 +16,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import com.prenota24.backend.auth.JwtAuthenticationEntryPoint;
+import com.prenota24.backend.auth.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableMethodSecurity
