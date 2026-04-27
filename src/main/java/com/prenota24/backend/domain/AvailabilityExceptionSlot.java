9322d3d7b840a208d1e1dd49f92a7f94da020100
CANCELLARO GIANMICHELE <gnmcl01@outlook.it>
2026-04-27 23:36:35 +0200

    refactor: replace single availability exception range with support for multiple granular time slots

---
+++ src/main/java/com/prenota24/backend/domain/AvailabilityExceptionSlot.java
@@ -0,0 +1,4 @@
+package com.prenota24.backend.domain;
+
+public class AvailabilityExceptionSlot {
+}
---
+++ src/main/java/com/prenota24/backend/dto/AvailabilityExceptionSlotResponse.java
@@ -0,0 +1,4 @@
+package com.prenota24.backend.dto;
+
+public record AvailabilityExceptionSlotResponse() {
+}
