b94030be96fea07ef404285870b17de66120ad5e
CANCELLARO GIANMICHELE <gnmcl01@outlook.it>
2026-04-29 00:07:46 +0200

    feat: enable Spring Data web support for DTO serialization

--- src/main/java/com/prenota24/backend/Prenota24BackendApplication.java
+++ src/main/java/com/prenota24/backend/Prenota24BackendApplication.java
@@ -3,9 +3,11 @@ package com.prenota24.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.prenota24.backend.config")
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Prenota24BackendApplication {
 
 	public static void main(String[] args) {
